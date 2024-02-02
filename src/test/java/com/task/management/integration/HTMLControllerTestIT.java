0443999a88b92d82ba1f63292ef7fbaa0582e95d
saadzafar659 <saadzafar659@gmail.com>
2024-02-02 23:53:57 +0100

    Reporting

---
+++ src/test/java/com/task/management/integration/HTMLControllerTestIT.java
@@ -0,0 +1,82 @@
+package com.task.management.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.task.management.entity.TaskEntity;
+import com.task.management.model.TaskDto;
+import com.task.management.repository.TaskRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ComponentScan("com.task.management")
+@Testcontainers
+public class HTMLControllerTestIT {
+	@Container
+	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("bookman")
+			.withPassword("task").withDatabaseName("task");
+
+	@LocalServerPort
+	private int port;
+	
+	
+	@DynamicPropertySource
+	static void databaseProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
+		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", mySQLContainer::getUsername);
+		registry.add("spring.datasource.password", mySQLContainer::getPassword);
+	}
+	
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TaskRepository taskRepository;
+    
+	@Test
+    public void testSaveContact() {
+        // Given
+        TaskDto taskDto = new TaskDto();
+        taskDto.setName("Test Contact");
+        taskDto.setAuthor("Test Creator");
+        taskDto.setPrice(50);
+
+        HttpEntity<TaskDto> request = new HttpEntity<>(taskDto);
+
+        // When
+        ResponseEntity<Void> responseEntity = restTemplate.exchange(
+                "http://localhost:" + port + "/saveTask",
+                HttpMethod.POST,
+                request,
+                Void.class
+        );
+
+        // Then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FOUND);     }
+}
+
+	
---
+++ src/test/java/com/task/management/integration/TaskControllerTestIT.java
@@ -0,0 +1,66 @@
+package com.task.management.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.task.management.model.TaskDto;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ComponentScan("com.task.management")
+@Testcontainers
+public class TaskControllerTestIT {
+	@Container
+	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("bookman")
+			.withPassword("task").withDatabaseName("task");
+
+	@LocalServerPort
+	private int port;
+	
+	
+	@DynamicPropertySource
+	static void databaseProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
+		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", mySQLContainer::getUsername);
+		registry.add("spring.datasource.password", mySQLContainer::getPassword);
+	}
+	
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    public void testCreateTask() throws Exception {
+        TaskDto taskDto = new TaskDto();
+        taskDto.setName("Test");
+        taskDto.setAuthor("Test");
+        taskDto.setPrice(50);
+
+        mockMvc.perform(post("/books")
+                .content(objectMapper.writeValueAsString(taskDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Test"))
+                .andExpect(jsonPath("$.author").value("Test"))
+                .andExpect(jsonPath("$.price").value(50));
+    }
+}
